c92682fcc48edad1a4ebfab1e7efee9299cd06be
Venkat Ragavan <mail2venkat21@gmail.com>
2021-02-10 20:16:03 +0530

    Added delete req for location data

--- routes/api/location.js
+++ routes/api/location.js
@@ -5,30 +5,27 @@ arr = []
 const Location = require('../../models/locationData');
 
 router.post('/',(req,res)=>{
-    const deviceID = req.body.deviceID;
-    const lat = req.body.latitude;
-    const lon = req.body.longitude;
-    const query = { deviceID: deviceID };
-    const update = { $set: { deviceID: deviceID, latitude: lat, longitude: lon}};
+    const query = { deviceID: req.body.deviceID };
+    const update = { $set: { deviceID: req.body.deviceID, latitude: req.body.latitude, longitude: req.body.longitude}};
     const options = { upsert: true };
     Location.updateOne(query, update, options, function (err, docs) { 
-        if (err){ 
-            res.send("error")
-        } 
-        else{ 
-            res.send("updated")
-        } 
+        if (err){res.send("error in updation")} 
+        else{res.send("updated records")} 
     });
 })
 
-router.get('/:id',(req,res)=>{
+router.get('/:id',async (req,res)=>{
     const id = req.params.id;
-    arr.forEach(function(element){
-        if(element[0]==id){
-            res.send(element)
-        }
+    arr = await Location.find({deviceID: id})
+    res.send(arr)
+})
+
+router.delete('/:id',(req,res)=>{
+    const id = req.params.id;
+    Location.deleteMany({deviceID: id},function(err,doc){
+        if (err){res.send("error in deletion")} 
+        else{res.send("deleted records")} 
     })
-    res.send([])
 })
 
 module.exports = router;
