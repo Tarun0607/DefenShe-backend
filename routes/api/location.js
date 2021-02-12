0f7fee90b2895e06ac91f70926201797c2177e5f
Venkat Ragavan <mail2venkat21@gmail.com>
2021-02-12 09:47:02 +0530

    Clean up

--- routes/api/location.js
+++ routes/api/location.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const path = require('path');
-const { route } = require("./trigger");
 
 const Location = require(path.join(__dirname,'..','..', 'models','locationData'));
 router.post('/',(req,res)=>{
