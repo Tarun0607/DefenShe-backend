1f6dd4dae9f4ca6c231542841fcf07067f45f9c4
Venkat Ragavan <mail2venkat21@gmail.com>
2021-04-16 17:55:07 +0530

    Update feed.js

--- routes/api/feed.js
+++ routes/api/feed.js
@@ -7,10 +7,37 @@ const News = require(path.join(__dirname,'..','..', 'models','feedData'));
 
 async function fetchNewsFeeds(){
     console.log("fetching feed")
-    const response = await newsapi.v2.everything({q: 'women security',language: 'en',});
-    const newsItem = [];
-    News.deleteMany({})
-    .then((msg)=>{
+    newsapi.v2.everything({q: 'women-security',language: 'en', sortBy:'relevancy', page: 1})
+    .then((response)=>{
+        const newsItem = [];
+        News.deleteMany({})
+        .then((msg)=>{
+            response.articles.forEach(article=>{
+                const item = {
+                    title: article.title,
+                    description: article.description,
+                    imageUri: article.urlToImage,
+                    url: article.url,
+                    date: article.publishedAt,
+                }
+                newsItem.push(item);
+            })
+            News.insertMany(newsItem)
+            .then((resp)=>{
+            })
+            .catch((err)=>{
+            });
+        })
+        .catch(err=>{
+            console.log("Unable to fetch news")
+        });
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+    newsapi.v2.everything({q: 'women-security',language: 'en', sortBy:'relevancy', page: 2})
+    .then((response)=>{
+        const newsItem = [];
         response.articles.forEach(article=>{
             const item = {
                 title: article.title,
@@ -28,10 +55,11 @@ async function fetchNewsFeeds(){
         });
     })
     .catch(err=>{
-        console.log("Unable to fetch news")
-    });
+        console.log(err)
+    })
+    
 }
-setInterval(fetchNewsFeeds, 900000);
+setInterval(fetchNewsFeeds, 9000);
 
 router.get('/fetch',async (req,res)=>{
     News.find({})
